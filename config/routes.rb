08a575907fffb0de1ccd3bc9425b77970d9755d3
Karina Sakata <karina.msakata@hotmail.com>
2022-10-26 18:21:41 -0300

    First Commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :product_categories, only: [:create, :show] do
-    resources :products, only: [:create]
-  end
-  devise_for :users
-  root 'welcome#index'
+  # resources :products
+  # resources :product_categories, only: [:create, :show] do
+  #   resources :products, only: [:create]
+  # end
+  # devise_for :users
+    root 'home#index'
 end
