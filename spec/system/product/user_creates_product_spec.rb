41cb0ac7f5941d186253e86985dfd26d91bd560b
Petra <petragrunheidt@gmail.com>
2022-10-26 18:37:14 -0300

    adds devise_methods
    Co-Authored-by: anyluvizott <any.luvizott@gmail.com>

--- spec/system/product/user_creates_product_spec.rb
+++ spec/system/product/user_creates_product_spec.rb
@@ -2,11 +2,11 @@ require 'rails_helper'
 
 describe 'Usuario acessa página de cadastrar produto' do
     it 'e não está autenticado' do
-        ProductCategory.create!(name: 'Categoria_1')
+        
 
         visit root_path
         click_on 'Cadastrar Produto'
 
         expect(current_path).to eq new_user_session_path
-    end 
+    end
 end
