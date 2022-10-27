c738655df2f7c24f5eb6193978dbfad4860127fd
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-10-27 14:15:46 -0300

    has_many no model, teste de registro e rotas

--- spec/system/product/register_product_spec.rb
+++ spec/system/product/register_product_spec.rb
@@ -5,17 +5,21 @@ describe "Usuário cadastra novo produto" do
   it 'com sucesso' do
     #arrange
     user = User.create!(email:'user@email.com', password: 'password') 
+    product_category = ProductCategory.create!(name: 'Geladeira')
     #act
     login_as(user)
     visit root_path
+    click_on('Ver Categorias de Produtos')
+    click_on('Geladeira')
+    
     click_on('Cadastrar Produto')
     fill_in "Nome",	with: "Geladeira Smart" 
-    fill_in "Preço",	with: 15.500
+    fill_in "Valor",	with: 15500
     click_on 'Salvar'
     #assert
     expect(page).to have_content 'Descrição do Produto'
     expect(page).to have_content 'Geladeira Smart'
-    expect(page).to have_content 'Preço: R$15.500,00'
+    expect(page).to have_content 'Preço: R$15500.0'
   end
 
   it 'com campos incompletos' do
