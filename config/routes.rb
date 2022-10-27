b2dd77e9f43244296348f3439a92afdaf60e71a8
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-10-27 12:18:46 -0300

    ajuste no devise e suas views

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
     resources :products, only: [:create]
   end
   devise_for :users
-  root 'welcome#index'
+  root 'home#index'
 end
--- spec/system/product/register_product_spec.rb
+++ spec/system/product/register_product_spec.rb
@@ -4,7 +4,7 @@ describe "Usuário cadastra novo produto" do
   
   it 'com sucesso' do
     #arrange
-    user = User.create!(email:'user@email.com', password: 'password')
+    user = User.create!(email:'user@email.com', password: 'password') 
     #act
     login_as(user)
     visit root_path
@@ -16,5 +16,19 @@ describe "Usuário cadastra novo produto" do
     expect(page).to have_content 'Descrição do Produto'
     expect(page).to have_content 'Geladeira Smart'
     expect(page).to have_content 'Preço: R$15.500,00'
+  end
+
+  it 'com campos incompletos' do
+    #arrange
+    user = User.create!(email:'user@email.com', password: 'password')
+    #act
+    login_as(user)
+    visit root_path
+    click_on('Cadastrar Produto')
+    fill_in "Nome",	with: "Geladeira Smart" 
+    fill_in "Preço",	with: nil
+    click_on 'Salvar'
+    #assert
+    expect(page).to have_content 'Não foi possível cadastrar esse produto'
   end
 end
