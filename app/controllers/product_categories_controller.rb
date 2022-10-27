b25ea698a783899bb183160ace74883726f1f800
Luiza Rodrigues <luiza.oceanoufc@gmail.com>
2022-10-27 14:14:10 -0300

    Ajustes nos controllers de product_category e product, metodos new, create e show

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -1,14 +1,28 @@
 class ProductCategoriesController < ApplicationController
-  def create
-
+  
+  def index
+    @product_categories = ProductCategory.all
   end
 
-  def search 
+  def new
+    @product_category = ProductCategory.new
+  end
 
+  def create
+    @product_category = ProductCategory.new(product_category_params)
+    if @product_category.save
+      redirect_to root_path, notice: 'Cadastrado com sucesso'
+    else
+      render 'new', notice: 'Não foi possível cadastrar produto'
+    end
+  end
+  
+  def show
+    @product_category = ProductCategory.find(params[:id])
   end
 
   private 
-
-  def find_available_transports()
+  def product_category_params
+    params.require(:product_category).permit(:name)
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show edit update destroy ]
+  #before_action :set_product, only: %i[ show edit update destroy ]
 
   # GET /products
   def index
@@ -8,10 +8,14 @@ class ProductsController < ApplicationController
 
   # GET /products/1
   def show
+    @product = Product.find(params[:id])
+    @product_category = ProductCategory.find(params[:product_category_id])
   end
 
   # GET /products/new
   def new
+    @product_category = ProductCategory.find(params[:product_category_id])
+    @product = Product.new
   end
 
   # GET /products/1/edit
@@ -20,6 +24,17 @@ class ProductsController < ApplicationController
 
   # POST /products
   def create
+    product_params = params.require(:product).permit(:name, :price)
+    @product_category = ProductCategory.find(params[:product_category_id])
+    @product = Product.new(product_params)
+    @product.product_category_id = @product_category.id
+    
+    if @product.save 
+      redirect_to product_category_product_path(@product_category.id, @product.id ), notice: 'Cadastrado com sucesso'
+    else
+      render 'new', notice: 'Não foi possível cadastrar produto'
+    end
+    
   end
 
   # PATCH/PUT /products/1
@@ -40,6 +55,6 @@ class ProductsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def product_params
-      params.require(:product).permit(:name, :price, :product_category_id)
+      params.require(:product).permit(:name, :price)
     end
 end
