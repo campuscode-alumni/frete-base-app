8585bdf7c6e482d8495c1a96b5d303b2171733a9
lcallefe <luciana_donadio@hotmail.com>
2022-10-29 17:33:41 -0300

    Teste para cadastro de produto -- campos obrigatórios
    Co-Authored-By: Sarah Santos<sarahsantosst@gmail.com>

--- spec/system/product/user_register_product_spec.rb
+++ spec/system/product/user_register_product_spec.rb
@@ -20,4 +20,25 @@ describe 'Usuário cadastra produto' do
     expect(page).to have_content("Modelo de produto: Eletrônicos")
     expect(page).to have_link("Voltar")
   end 
+
+  it 'e mantém campos obrigatórios' do  
+    user = User.create!(email:'joao@campuscode.com', password:'12345678')
+    ProductCategory.create!(name:'Eletrodométicos')
+  
+    login_as(user)
+    visit root_path
+    click_on 'Produtos'
+    click_on 'Cadastrar produto'
+    fill_in 'Nome', with: ''
+    fill_in 'Preço', with: ''
+    click_on 'Salvar'
+  
+    expect(page).to have_content("Nome não pode ficar em branco")
+    expect(page).to have_content("Preço não pode ficar em branco")
+    expect(page).to have_link("Voltar")
+    expect(page).not_to have_content("Eletrodométicos")
+    expect(page).not_to have_content("Preço")
+    expect(page).not_to have_content("Nome")
+    expect(current_path).to eq new_product_path
+  end
 end
